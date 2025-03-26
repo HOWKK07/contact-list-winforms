8a1201fb83b36d260270ea5e555e14ce4d6bcf28
NguyenThinh <thinhnguyenkk07@gmail.com>
2025-03-26 13:14:56 +0700

    Sửa Form thêm liên hệ, bổ sung trường Email

--- ContactList/ContactList/AddContactForm.Designer.cs
+++ ContactList/ContactList/AddContactForm.Designer.cs
@@ -31,39 +31,52 @@
             this.txtHoTen = new System.Windows.Forms.TextBox();
             this.txtSDT = new System.Windows.Forms.TextBox();
             this.btnLuu = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtHoTen
             // 
-            this.txtHoTen.Location = new System.Drawing.Point(205, 108);
+            this.txtHoTen.Location = new System.Drawing.Point(161, 73);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(2);
             this.txtHoTen.Name = "txtHoTen";
-            this.txtHoTen.Size = new System.Drawing.Size(100, 26);
+            this.txtHoTen.Size = new System.Drawing.Size(132, 20);
             this.txtHoTen.TabIndex = 0;
             // 
             // txtSDT
             // 
-            this.txtSDT.Location = new System.Drawing.Point(222, 258);
+            this.txtSDT.Location = new System.Drawing.Point(160, 117);
+            this.txtSDT.Margin = new System.Windows.Forms.Padding(2);
             this.txtSDT.Name = "txtSDT";
-            this.txtSDT.Size = new System.Drawing.Size(100, 26);
+            this.txtSDT.Size = new System.Drawing.Size(133, 20);
             this.txtSDT.TabIndex = 1;
             // 
             // btnLuu
             // 
-            this.btnLuu.Location = new System.Drawing.Point(509, 199);
+            this.btnLuu.Location = new System.Drawing.Point(339, 129);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(2);
             this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.Size = new System.Drawing.Size(68, 36);
             this.btnLuu.TabIndex = 2;
             this.btnLuu.Text = "Lưu";
             this.btnLuu.UseVisualStyleBackColor = true;
             // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(161, 160);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(132, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
             // AddContactForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.btnLuu);
             this.Controls.Add(this.txtSDT);
             this.Controls.Add(this.txtHoTen);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "AddContactForm";
             this.Text = "AddContactForm";
             this.ResumeLayout(false);
@@ -76,5 +89,6 @@
         private System.Windows.Forms.TextBox txtHoTen;
         private System.Windows.Forms.TextBox txtSDT;
         private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.TextBox txtEmail;
     }
 }
